e3de22047831144970a11af2165cf5af0a1822af
Long Chung Chan <HarryChan@v1020-wn-226-1.campus-dynamic.uwaterloo.ca>
2016-06-26 03:35:52 -0400

    renew the company page
    /
    /
    '

--- CompanyInfo.php
+++ CompanyInfo.php
@@ -2,9 +2,30 @@
 	include 'loginLibrary/connection.php';
 	include 'functions.php';
 	$CompName = $_GET['comName'];
-	echo $CompName;
-
-	show_company($CompName, $link);
-	showReview($CompName, $link);
-
 ?>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="style/style_main.css">
+	</head>
+
+	<body>
+		<ul>
+  			<li><a href="">Hi! <?php
+  			session_start();
+  			echo $_SESSION['login_user']
+  			?></li>
+  			<li style="float:right"><a href="main.php">logout</a></li>
+		</ul>
+
+		<br><br>
+		<div class="centering">
+			<h2><?php echo $CompName?></h2>
+			<h3>Basic Information:</h3>
+
+		</div>
+
+
+	</body>	
+</html>
