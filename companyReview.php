115c411b301e960f2eb091b86498ed5f177e48de
Arthur L <pspkazy@gmail.com>
2016-06-26 04:05:25 -0400

    post review form

---
+++ companyReview.php
@@ -0,0 +1,40 @@
+<?php
+  include 'loginLibrary/connection.php';
+  include 'functions.php';
+  $CompName = $_GET['comName'];
+  echo $CompName;
+
+  show_company($CompName, $link);
+  showReview($CompName, $link);
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style/style_reviewform.css">
+  </head>
+
+  <body>  
+  <div class="centering">
+    <h1>Add a review for this employer</h1>
+
+    <div id = "reviewtextarea">
+    <textarea name="input47" rows="3" cols="10">
+    Your multiline text is here.
+    </textarea>
+                <input type = "submit"   id = "postReview"  value = "Review" name="submit">
+            </form>
+    </div>
+
+        <br><br>
+          <?php
+             if(isset($_POST['post_review'])){
+              post_review($_POST['post_review'],$link);
+             }
+          ?>
+        </form>
+
+  </div>
+  </body>
+</html>
